acc33fd8b8e48f9fcdc5c8625823f7ae99020b21
hamza05628 <hamidi.hamza@ofppt-edu.ma>
2024-12-03 17:32:00 +0100

    new

--- api/index.php
+++ api/index.php
@@ -1,2 +1,11 @@
 <?php
-require __DIR__ . '/../public/index.php';
+// Load the Laravel application
+require __DIR____ . '/../vendor/autoload.php';
+$app = require_once _DIR_. '/../bootstrap/app.php';
+// Run the application
+$kernel = $app->make(Illuminate\Contracts\Http\Kernel::class);
+$response = $kernel->handle(
+$request = Illuminate\Http\Request::capture()
+);
+$response->send();
+$kernel->terminate($request, $response);
